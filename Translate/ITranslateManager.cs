5182862e32539341daf129116cba860c67a6a5ec
Jiří Dobrkovský <dobrkovsky@gmail.com>
2015-05-09 16:09:17 +0200

    CreateProject

---
+++ Translate/ITranslateManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Translate
+{
+    interface ITranslateManager
+    {
+        string GetString(string project, string key, string laguage);
+    }
+}
---
+++ UnitTestProject1/TranslateLibTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using Translate;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class TranslateLibTest
+    {
+        const string path = "";
+        TranslateManager manager = new TranslateManager(path);
+        
+        [Test]
+        public void InitManagerTest()
+        {
+            var manager = new TranslateManager(path);
+            Assert.AreEqual(manager.IsReady, true);
+        }
+
+        [Test]
+        public void successfulTranslateTest()
+        {
+            var resultString = manager.GetString("elsa", "main.header", "us");
+            Assert.AreEqual(resultString, "Elsa - new page");
+            resultString = manager.GetString("elsa", "main.header", "cz");
+            Assert.AreEqual(resultString, "Elsa - new page");            
+        }
+
+        [Test]
+        public void failLanguageTranslateTest()
+        {
+            var resultString = manager.GetString("elsa", "main.header", "de");
+            Assert.AreEqual(resultString, "Elsa - new page");
+        }
+
+        [Test]
+        public void failKeyTranslateTest()
+        {
+            var resultString = manager.GetString("elsa", "main.headerrr", "us");
+            Assert.AreEqual(resultString, "Elsa - new page");
+        }
+    }
+}
